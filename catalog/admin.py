9c8a1b423c4a07b0ee8e4e3271677ee8dd2ab18a
willi8jk <willi8jk@dukes.jmu.edu>
2026-09-09 20:46:07 -0400

    django admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+
+# consider adding save_as to more easily add instance that have similar values
+# you could do this for any or all of the below, e.g.
+# admin.site.register(Book, save_as=True)
+# except mayeb not bookinstance tho
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    # optionally enable "save as new", by uncommenting line below
+    # save_as = True
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
