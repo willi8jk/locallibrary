038d96c98ba9ff0a55675e2d13f8309188a2b799
willi8jk <willi8jk@dukes.jmu.edu>
2026-09-15 21:56:18 -0400

    prep03: home page

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,5 +11,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+  path('', views.index, name='index'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    
+    # Challenge: generate counts for genres and books that contain a particular word (case insensitive)
+    word = "Food"
+    num_genres_food = Genre.objects.filter(name__icontains=word).count()
+    num_books_food = Book.objects.filter(title__icontains=word).count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres_food': num_genres_food,
+        'num_books_food': num_books_food,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
